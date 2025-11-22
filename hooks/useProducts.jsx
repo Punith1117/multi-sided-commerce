22bd0e53c2a21fc0bcedf51ce4be30ea660237d9
Punith1117 <punith11172006@gmail.com>
2025-11-22 16:12:00 +0530

    feat(useProducts): add new custom hook to fetch products from database

---
+++ hooks/useProducts.jsx
@@ -0,0 +1,28 @@
+import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { databases } from '../lib/appwrite'
+
+const useProducts = () => {
+	const [products, setProducts] = useState([])
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				const result = await databases.listDocuments({
+					databaseId: '691c7cde002ac4b16fd8',
+					collectionId: 'products',
+				})
+				setProducts(result.documents)
+				setLoading(false)
+			} catch (e) {
+				console.log(e)
+			}
+		}
+		fetchProducts()
+	}, [])
+
+  	return {products, loading}
+}
+
+export default useProducts
