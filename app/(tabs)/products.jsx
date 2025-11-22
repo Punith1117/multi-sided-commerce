88720b9381feabae4c6f626d68c7506b73712cb8
Punith1117 <punith11172006@gmail.com>
2025-11-22 16:12:38 +0530

    feat(products): list all products with useProducts hook and ProductCard component

--- app/(tabs)/products.jsx
+++ app/(tabs)/products.jsx
@@ -1,13 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import ProductCard from '../../components/ProductCard'
+import useProducts from '../../hooks/useProducts'
 
 const Products = () => {
-  return (
-    <SafeAreaView>
-      <Text>Products</Text>
-    </SafeAreaView>
-  )
+	const safeArea = useSafeAreaInsets()
+	const {products, loading} = useProducts()
+
+	if (loading) return (
+		<View style={{marginTop: safeArea.top}}>
+			<Text>Loading...</Text>
+		</View>
+	)
+	
+	return (
+		<View style={{marginTop: safeArea.top}}>
+			<FlatList 
+				data={products}
+				renderItem={({item}) => <ProductCard details={item}/>}
+				keyExtractor={product => product.id}
+			/>
+		</View>
+	)
 }
 
 export default Products
