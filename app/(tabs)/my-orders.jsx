c0da1b3c568a63bfe07e3d012837f74733e0a72a
Punith1117 <punith11172006@gmail.com>
2025-11-24 23:59:26 +0530

    feat(my-orders): fetch orders from database and display using FlatList

--- app/(tabs)/my-orders.jsx
+++ app/(tabs)/my-orders.jsx
@@ -1,20 +1,67 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import useUser from '../../hooks/useUser'
 import UserOnly from '../../components/UserOnly'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { Link } from 'expo-router'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { supabase } from '../../lib/supabase'
+import OrderCard from '../../components/OrderCard'
 
 const MyOrders = () => {
 	const {user, userLoading} = useUser()
-	if (userLoading) return <Text>Loading</Text>
+	const safeArea = useSafeAreaInsets()
+	const [orders, setOrders] = useState([])
+	const [ordersLoading, setOrdersLoading] = useState(false)
+	
+	useEffect(() => {
+		const fetchOrders = async () => {
+			setOrdersLoading(true)
+			const {data, error} = await supabase
+				.from('orders')
+				.select(`
+					id,
+					productId,
+					customerId,
+					deliveryPerson:deliveryPersonId(email),
+					statusId,
+					created_at,
+					otp,
+					status (
+						meaning
+					),
+					products (
+						name,
+						price
+					)
+				`)
+				.eq('customerId', user.$id)
+				.order('created_at', {ascending: false})
+			if (error)
+				console.log(error)
+			else  
+				setOrders(data)
+			setOrdersLoading(false)
+		}
+		if (!userLoading && user) fetchOrders()
+	}, [user, userLoading])
+	
+	const cancelOrder = async (id) => {
+		// to do
+	}
+
+	if (userLoading || ordersLoading) return <Text>Loading</Text>
+
 	return (
 		<UserOnly>
-			<SafeAreaView>
-				<Text>MyOrders</Text>
+			<View style={{marginTop: safeArea.top, marginBottom: safeArea.bottom}}>
 				<Text>Hello {user?.email}</Text>
+				<FlatList 
+					data={orders}
+					renderItem={({item}) => <OrderCard details={item} cancelOrder={cancelOrder}/>}
+					keyExtractor={order => order.id.toString()}
+				/>
 				<Link href={'/logout'}>Logout</Link>
-			</SafeAreaView>
+			</View>
 		</UserOnly>
 	)
 }
